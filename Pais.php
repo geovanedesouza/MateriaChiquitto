f6c62b06eafee72e143425515cff107557eab4ba
Hugo Garcia <hugosg50@hotmail.com>
2014-04-01 21:13:45 -0300

    update hugo

--- Pais.php
+++ Pais.php
@@ -1,1 +1,19 @@
 <?php 
+
+  class Pais{
+		private $codIso;
+		private $nome;
+		private $populacao;
+		private $dimensao;
+		private $fronteira = new Array();
+		
+		public function setFronteira(array $paises){
+			foreach($paises as $v){
+				$this->fronteira[] = $v;
+			}
+			//$this->fronteira = array_merge($this->fronteira,$paises);
+		}
+		
+		public function setFronteira1($pais){
+			$this->fronteira[] = $pais;
+		}
