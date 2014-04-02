55c0fb9f6eed5662fa2260723f8ee922d11b1385
vaneht <harumivane@gmail.com>
2014-04-01 21:25:41 -0300

    Update Vanessa

--- Pais.php
+++ Pais.php
@@ -13,3 +13,27 @@
 			}
 		}
 		
+		
+		//c) Um método que permita verificar se dois objetos representam o mesmo país (igualdade semântica). Dois países são iguais se tiverem o mesmo código ISO;
+		public function comparaPais($p1, $p2){
+			if($p1->codIso == $p2->codIso){
+				return true;
+			}else{
+				return false;
+			}
+		}
+		
+		//d) Um método que informe se outro país é limite do pais que recebeu por parametro;
+		public function paisLimite($p1){
+			foreach($this->fronteira as $v){
+				if($v == $p1){
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		//e) Um método que retorne a densidade populacional do país;
+		public function densidadePop($p1){
+			return ($p1->populacao / $p1->dimensao);
+		}
